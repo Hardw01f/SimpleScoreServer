3b4b56c3847d109a3dc696c1a2427614f3dcc749
Hardwolf <hardwolf@Hardwolfs-MacBook-Pro.local>
2020-08-05 19:53:11 +0900

    feat: 🎸 add first app verifed

---
+++ pkg/conn/conn.go
@@ -0,0 +1,74 @@
+package conn
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/labstack/echo/v4"
+	"golang.org/x/xerrors"
+)
+
+type ScoreParameters struct {
+	Server string
+	Score  int
+}
+
+type Score struct {
+	gorm.Model
+	Server string
+	Score  int
+}
+
+func ScoreInit() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		db, err := gorm.Open("mysql", "root:toor@(mysql)/nanohardening?charset=utf8&parseTime=True&loc=Local")
+		if err != nil {
+			err = xerrors.New("Failed database connection")
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		defer db.Close()
+
+		db.Set("gorm:table_options", "ENGINE=InnoDB")
+		db.AutoMigrate(&Score{})
+		return c.String(http.StatusOK, "Migrate Successful")
+	}
+}
+
+func AddScore() echo.HandlerFunc {
+	return func(c echo.Context) error {
+		db, err := gorm.Open("mysql", "root:toor@(mysql)/nanohardening?charset=utf8&parseTime=True&loc=Local")
+		if err != nil {
+			err = xerrors.New("Failed database connection")
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+		defer db.Close()
+
+		s := new(ScoreParameters)
+		if err = c.Bind(s); err != nil {
+			err = xerrors.New("Could not parse parameters")
+			return c.String(http.StatusInternalServerError, err.Error())
+		}
+
+		var score Score
+		var count int
+		db.Where("server = ?", s.Server).Find(&score).Count(&count)
+
+		fmt.Println(count)
+
+		score.Server = s.Server
+		score.Score = s.Score
+
+		if count == 0 {
+			db.Create(&score)
+		} else {
+			var originScore Score
+			db.First(&originScore)
+			db.Model(&score).Where("server = ?", score.Server).Update("score", originScore.Score+score.Score)
+		}
+
+		return c.JSON(http.StatusOK, s)
+
+	}
+}
---
+++ pkg/conn/go.mod
@@ -0,0 +1,9 @@
+module github.com/Hardw01f/SimpleScoreServer/pkg/conn
+
+go 1.13
+
+require (
+	github.com/jinzhu/gorm v1.9.15
+	github.com/labstack/echo/v4 v4.1.16
+	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
+)
