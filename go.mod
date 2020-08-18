e9851af3edaee2137af755030305b0cfdd518d5c
Hardwolf <hardwolf@Hardwolfs-MacBook-Pro.local>
2020-08-18 14:47:35 +0900

    feat: 🎸 add logging feature

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.13
 require (
 	github.com/Hardw01f/SimpleScoreServer/pkg/conn v0.0.0-00010101000000-000000000000
 	github.com/jinzhu/gorm v1.9.15
+	github.com/labstack/echo v3.3.10+incompatible
 	github.com/labstack/echo/v4 v4.1.16
 	golang.org/x/xerrors v0.0.0-20191204190536-9bdfabe68543
 )
--- main.go
+++ main.go
@@ -2,14 +2,25 @@ package main
 
 import (
 	"net/http"
+	"os"
 
 	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
 
 	"github.com/Hardw01f/SimpleScoreServer/pkg/conn"
 )
 
 func main() {
 	e := echo.New()
+
+	e.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
+		Skipper: middleware.DefaultSkipper,
+		Format: `{"time":"${time_rfc3339_nano}","id":"${id}","remote_ip":"${remote_ip}","host":"${host}",` +
+			`"method":"${method}","uri":"${uri}","status":${status},"error":"${error}","latency":${latency},` +
+			`"latency_human":"${latency_human}","bytes_in":${bytes_in},` +
+			`"bytes_out":${bytes_out}}` + "\n",
+		Output: os.Stdout,
+	}))
 
 	e.GET("/test", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
